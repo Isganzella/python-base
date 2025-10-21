ed1cf5f0b0c4da41010128b47993901760a767a0
misganzella <misganzella@gmail.com>
2025-10-21 15:23:03 +0000

    Alterado

--- hello.py
+++ hello.py
@@ -1,4 +1,36 @@
+#!/usr/bin/env python3
+"""Hello World Multi Linguas.
 
-#Este programa imprime hello world
+Dependendo da lingua configurada no ambiente
+o programa exibe a mensagem correspondente.
 
-print('Marlos'.upper())
+Como usar:
+
+Tenha a váriavel LANG devidademente configurada ex:
+
+    export LANG=pt_BR
+
+Execução:
+
+    python3 heelo.py ou 
+    ./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "Marlos Isganzella"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+print(f"current_language: {current_language}")
+
+msg = "Hello, World!"
+
+if current_language == "pt_br":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+elif current_language == "es_SP":
+    msg = "Hola, Mundo!"
+
+print(msg)
