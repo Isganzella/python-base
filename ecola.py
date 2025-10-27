8228b342ae25d8839e8a620d10db14b57cef7916
Marlos Isganzella <misganzella@gmail.com>
2025-10-27 05:56:05 -0300

    Finalizado escript escola.py

---
+++ ecola.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python 3
+
+"""Exibe relatorio de criancas por atividades
+
+Imprimir a lista de criancas agrupadas por salas
+que frequentam cada umas das atividades
+
+"""
+
+__version__ = '0.1.0'
+
+#Dados
+
+sala1 = ['Erik','Maia', 'Gustavo', 'Manuel', 'Sofia', 'Joana']
+sala2 = ['Joao', 'Antonio', 'Carlos', 'Maria','Isolda']
+
+
+aula_ingles = ["Erik",'Maia', 'Joana', 'Carlos', 'Antonio']
+aula_musica = ['Erik', 'Sofia', 'Maria']
+aula_danca = ['Gustavo', 'Sofia', 'Joana', 'Antonio']
+
+atividades = [
+            ("Ingles", aula_ingles), 
+            ("Musica", aula_musica), 
+            ("Danca", aula_danca)
+]
+
+for nome_atividade, atividade in atividades:
+
+    atividade_sala1 = []
+    atividade_sala2 = []
+
+    for aluno in atividade:
+        if aluno in sala1:
+            atividade_sala1.append(aluno)
+        elif aluno in sala2:
+            atividade_sala2.append(aluno)
+
+    print(f'A {nome_atividade} sala 1:', atividade_sala1)
+    print(f'A {nome_atividade} sala 2:', atividade_sala2)
+    print('-' * 30)
