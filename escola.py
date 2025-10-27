d6125ab273da3f54164a3e61cd01e308234603a3
Marlos Isganzella <misganzella@gmail.com>
2025-10-26 21:37:35 -0300

    Criado script escola.py

---
+++ escola.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python 3
+
+"""Exibe relatorio de criancas por atividades
+
+Imprimir a lista de criancas agrupadas por salas
+que frequentam cada umas das atividades
+
+"""
+
+__version__ = '0.1.0'
+
+#Dados
+
+sala1 = ['Erik','Maria', 'Gustavo', 'Manuel', 'Sofia', 'Joana']
+sala2 = ['Joao', 'Antonio', 'Carlos', 'Maria','Isolda']
+
+
+aula_ingles = ["Erik",'Maia', 'Joana'. 'Carlos', 'Antonio']
+aula_musica = ['Erik', 'Sofia'. 'Maria']
+aula_danca = ['Gustavo'. 'Sofia'. 'Joana'. 'Antonio']
+
+
+# Imprimir para cada atividada os alunos de cada atividade.
+
+aula_ingles_sala1 = []
+aula_ingles_sala2 = []
+
