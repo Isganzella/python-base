62a27dc25f04ee33de96bbe71950d8d2911e9177
Marlos Isganzella <misganzella@gmail.com>
2025-10-29 05:25:48 -0300

    Refatorado o codigo para uso com sets

--- ecola.py
+++ ecola.py
@@ -27,14 +27,14 @@ atividades = [
 
 for nome_atividade, atividade in atividades:
 
-    atividade_sala1 = []
-    atividade_sala2 = []
+    atividade_sala1 = set(sala1) & set(atividade)
+    atividade_sala2 = set(sala2) & set(atividade)
 
-    for aluno in atividade:
+    """ for aluno in atividade:
         if aluno in sala1:
             atividade_sala1.append(aluno)
         elif aluno in sala2:
-            atividade_sala2.append(aluno)
+            atividade_sala2.append(aluno)"""
 
     print(f'A {nome_atividade} sala 1:', atividade_sala1)
     print(f'A {nome_atividade} sala 2:', atividade_sala2)
